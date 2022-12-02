ab626fc80a3bc84236c9aa9af95e1f3f91f2a2e6
Brandon.DeVore <Brandon.DeVore@publix.com>
2022-12-02 08:26:02 -0500

    Images to static

---
+++ src/components/sections/HeroSection.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+function HeroSection() {
+  return (
+    <>
+      <img src="./images/logos/logo.svg" alt="logo" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </>
+  )
+}
+
+export default HeroSection
