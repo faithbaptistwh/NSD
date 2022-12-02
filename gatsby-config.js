f7f4be75e88e12eb5530ae42792b8de856579468
Brandon.DeVore <Brandon.DeVore@publix.com>
2022-12-02 00:53:51 -0500

    First Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Next Step Discipleship`,
+    description: `Next Step Discipleship`,
+    author: `Brandon DeVore`,
   },
   plugins: [
     `gatsby-plugin-image`,
