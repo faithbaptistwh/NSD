cccb4d34defed78b787712fb10c628d00ad1ba57
Brandon.DeVore <Brandon.DeVore@publix.com>
2022-12-06 07:17:14 -0500

    Added Menu and Menu Data

---
+++ src/data/menuData.js
@@ -0,0 +1,5 @@
+export const menuData = [
+  { title: "Courses", icon: "images/icons/courses.svg", link: "/courses" },
+  { title: "About", icon: "images/icons/account.svg", link: "/courses" },
+  { title: "Contact", icon: "images/icons/pen.svg", link: "/courses" },
+]
